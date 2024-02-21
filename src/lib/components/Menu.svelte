295fc655566e8ccfae181f455800ed35e6a4b510
hikaru90 <alexbueckner@gmail.com>
2024-02-22 00:26:57 +0100

    change deploy action

---
+++ src/lib/components/Menu.svelte
@@ -0,0 +1,33 @@
+<script>
+  // Import any necessary dependencies
+  import { onMount } from 'svelte';
+
+  // Define component properties
+  export let menuItems = [
+    { label: 'Home' },
+    { label: 'Kontakt' },
+  ];
+
+  // Define any component functions or lifecycle hooks
+  onMount(() => {
+    // Any initialization logic can go here
+  });
+
+  // Define any other component-specific functions
+  function handleMenuItemClick(item) {
+    // Handle click event for menu items
+    console.log('Clicked:', item);
+  }
+</script>
+
+<!-- Component HTML structure -->
+<nav class="flex items-center justify-between max-container">
+  <div>
+    Empathy Link
+  </div>
+  <div class="flex items-center">
+    {#each menuItems as item}
+      <div on:click={() => handleMenuItemClick(item)}>{item.label}</div>
+    {/each}
+  </div>
+</nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
+<script>
+  import { ChevronRight } from "radix-icons-svelte";
+  import { Button } from '$lib/components/ui/button'
+  import { Sun, Moon } from "radix-icons-svelte";
+  import { toggleMode } from "mode-watcher";
+
+  import Menu from '$lib/components/Menu.svelte';
+</script>
+
+<Menu />
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Button variant="outline" size="icon">
+  <ChevronRight class="h-4 w-4" />
+</Button>
+
+<Button on:click={toggleMode} variant="outline" size="icon">
+  <Sun
+    class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+  />
+  <Moon
+    class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+  />
+  <span class="sr-only">Toggle theme</span>
+</Button>
+
+<style lang="scss">
+	:global(.max-container){
+    color: blue;
+  }
+</style>
